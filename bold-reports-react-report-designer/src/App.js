6fd0ada25f6c5845aef36d26be75e7382507f626
gpsakthivel <psakthi24vel@gmail.com>
2023-07-13 21:31:14 +0530

    Added React Bold reports projects

---
+++ bold-reports-react-report-designer/src/App.js
@@ -0,0 +1,31 @@
+/* eslint-disable */
+import React from 'react';
+import './App.css';
+//Report designer source
+import '@boldreports/javascript-reporting-controls/Scripts/bold.report-designer.min';
+import '@boldreports/javascript-reporting-controls/Scripts/bold.report-viewer.min';
+import '@boldreports/javascript-reporting-controls/Content/material/bold.reports.all.min.css';
+import '@boldreports/javascript-reporting-controls/Content/material/bold.reportdesigner.min.css';
+//Data-Visualization
+import '@boldreports/javascript-reporting-controls/Scripts/data-visualization/ej.bulletgraph.min';
+import '@boldreports/javascript-reporting-controls/Scripts/data-visualization/ej.chart.min';
+//Reports react base
+import '@boldreports/react-reporting-components/Scripts/bold.reports.react.min';
+
+var designerStyle = {
+  'height': '700px',
+  'width': '100%'
+};
+
+function App() {
+  return (
+    <div style={designerStyle}>
+      <BoldReportDesignerComponent
+      id="reportdesigner_container"
+      serviceUrl={'https://demos.boldreports.com/services/api/ReportDesignerWebApi'}>
+      </BoldReportDesignerComponent>
+    </div>
+  );
+}
+
+export default App;
---
+++ bold-reports-react-report-viewer/src/App.js
@@ -0,0 +1,27 @@
+/* eslint-disable */
+import React from 'react';
+import './App.css';
+//Report Viewer source
+import '@boldreports/javascript-reporting-controls/Scripts/bold.report-viewer.min';
+import '@boldreports/javascript-reporting-controls/Content/material/bold.reports.all.min.css';
+//Data-Visualization
+import '@boldreports/javascript-reporting-controls/Scripts/data-visualization/ej.bulletgraph.min';
+import '@boldreports/javascript-reporting-controls/Scripts/data-visualization/ej.chart.min';
+//Reports react base
+import '@boldreports/react-reporting-components/Scripts/bold.reports.react.min';
+
+var viewerStyle = {'height': '900px', 'width': '100%'};
+
+function App() {
+  return (
+    <div style={viewerStyle}>
+      <BoldReportViewerComponent
+      id="reportviewer-container"
+      reportServiceUrl = {'https://demos.boldreports.com/services/api/ReportViewer'}
+      reportPath = {'~/Resources/docs/sales-order-detail.rdl'}>
+      </BoldReportViewerComponent>
+    </div>
+  );
+}
+
+export default App;
